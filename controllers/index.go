f5d8fefd25fe795edc148f787304af80f800d2cf
Simon Niedermayr <simon.niedermayr@gmx.de>
2018-04-20 14:25:13 +0200

    fixed different topic list bug

--- controllers/index.go
+++ controllers/index.go
@@ -1,9 +1,5 @@
 package controllers
 
-import (
-	"github.com/KeKsBoTer/socialloot/models"
-)
-
 type IndexController struct {
 	AuthController
 }
@@ -11,10 +7,4 @@ type IndexController struct {
 func (this *IndexController) Get() {
 	this.Data["Title"] = "Socailloot: like reddit but different"
 	this.TplName = "pages/index.tpl"
-	var topics []*models.Topic
-	if _, err := models.Topics().All(&topics); err != nil {
-		this.Abort("505")
-		return
-	}
-	this.Data["Topics"] = topics
 }
